930358d43a4ee64f7bdca3a65d8d7f9d725a291b
Mikael Gustafsson <mikaelgustafsson@Mikaels-MacBook-Pro.local>
2022-10-06 20:35:09 +0300

    korjauksia emojiin

--- html/js/emoji.js
+++ html/js/emoji.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   $("#input").emojioneArea({
     pickerPosition: "bottom",
   });
+  $("#chatBtn").click(() => {
+    $(".emojionearea-editor").html("");
+  });
 });
